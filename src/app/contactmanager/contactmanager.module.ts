3f9d75f85c16b7a24eb8dfa832962767306ff4c8
Mufeed H. AlMatar <almatarm@gamil.com>
2021-05-28 06:12:50 +0100

    ContactManager Module Sidenav, App Toolbar with toggle button, Main Content Layout

---
+++ src/app/contactmanager/contactmanager.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactmanagerAppComponent } from './contactmanager-app.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
+
+const routes: Routes = [
+  {
+    path: '', component: ContactmanagerAppComponent,
+    children: [
+      { path: '', component: MainContentComponent }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  declarations: [
+    ContactmanagerAppComponent,
+    MainContentComponent,
+    SidenavComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ContactmanagerModule { }
